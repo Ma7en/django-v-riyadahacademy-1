aab58f372b1f3140821b70f16d6c50df78c517ff
Ma7en <m9ee9m@gmail.com>
2025-06-13 11:53:42 +0300

    fix

--- riyadahacademy/settings.py
+++ riyadahacademy/settings.py
@@ -177,13 +177,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS settings
 # CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:5174').split(',')
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:5174",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1:5173",
+# ]
 # CORS_ALLOWED_ORIGINS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 
